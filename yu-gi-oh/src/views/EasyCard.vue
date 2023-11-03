c65d749b4dd84c8a6793953b5bfd1c0d55b1a121
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2023-11-03 15:05:51 -0400

    finished basic functionality of program

--- yu-gi-oh/src/views/EasyCard.vue
+++ yu-gi-oh/src/views/EasyCard.vue
@@ -1,67 +1,128 @@
 <template>
-    <div class = "searching">
-      <input v-model="searchedCharacter" type="text">
-      <br>
-      <br>
-      <button @click="findCharacter">Find Character</button>
-      <h1>{{ character }}</h1>
-      <br>
-      <h1> {{ characterData }}</h1>
-      <p v-if="error">{{ error }}</p>  
+  <div class="quiz">
+    <button @click="getQuestion">Get Question</button>
+    <h1> Which card matches the following description:</h1>
+    <h2>{{ question }}</h2>
+    <ul>
+      <li @click="checkAnswer(option1)">{{ option1 }}</li>
+      <li @click="checkAnswer(option2)">{{ option2 }}</li>
+      <li @click="checkAnswer(option3)">{{ option3 }}</li>
+      <li @click="checkAnswer(option4)">{{ option4 }}</li>
+    </ul>
+    <div v-if="showResult">
+      <p>{{ resultMessage }}</p>
     </div>
+  </div>
 </template>
-  
+
 <script>
-  import axios from 'axios'
-  import { ref } from 'vue'
-  
-  export default {
-    //name: 'FindCard',
-    setup() {
-      const searchedCharacter = ref('')
-      const characterData = ref('')
-      const character = ref('')
-      const error = ref('')
-  
-      const findCharacter = async () => {
-        try 
-        {
-          error.value = '' // Clearing previous error message if any
-          const response = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', {
-            params: 
-            {
-              name: searchedCharacter.value // Finding the specific character
-            }
-          })
-          character.value = response.data.data[0].name //returns name
-          characterData.value = response.data.data[0].desc //returns description
-        } 
-        catch (e) 
-        {
-          character.value = ''
-          error.value = `Character not found: ${searchedCharacter.value}`
-        }
+import axios from 'axios';
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const types = ref([
+      'Skill Card',
+      'Spell Card',
+      'Trap Card',
+      'Normal Tuner Monster',
+      'Effect Monster',
+      'Tuner Monster',
+      'Flip Effect Monster',
+      'Union Effect Monster',
+      'Gemini Monster',
+      'Pendulum Effect Monster',
+      'Pendulum Normal Monster',
+      'Ritual Monster',
+      'Ritual Effect Monster',
+      'Toon Monster',
+      'Fusion Monster',
+      'Synchro Monster',
+      'XYZ Monster',
+    ]);
+
+    const option1 = ref('');
+    const option2 = ref('');
+    const option3 = ref('');
+    const option4 = ref('');
+    const question = ref('');
+    const correctAnswer = ref('');
+    const showResult = ref(false);
+    const resultMessage = ref('');
+    const chosenType = ref('');
+
+    const getQuestion = async () => {
+      try {
+        showResult.value = false;
+        chosenType.value = types.value[Math.floor(Math.random() * types.value.length)];
+        const response = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?type=${chosenType.value}`);
+        const data = response.data.data;
+        let x = Math.floor(Math.random() * data.length);
+        question.value = data[x].desc;
+        correctAnswer.value = data[x].name;
+        const options = [
+          option1.value = data[x].name,
+          data[Math.floor(Math.random() * data.length)].name,
+          data[Math.floor(Math.random() * data.length)].name,
+          data[Math.floor(Math.random() * data.length)].name,
+        ];
+        options.sort(() => Math.random() - 0.5); // Shuffle options
+        option1.value = options[0];
+        option2.value = options[1];
+        option3.value = options[2];
+        option4.value = options[3];
+      } catch (error) {
+        console.error(error);
       }
-  
-      return { 
-        searchedCharacter, 
-        character,
-        characterData,
-        findCharacter, 
-        error
+    };
+
+    const checkAnswer = (selectedOption) => {
+      if (selectedOption === correctAnswer.value) {
+        resultMessage.value = 'Correct!';
+      } else {
+        resultMessage.value = 'Incorrect. The correct answer is: ' + correctAnswer.value;
       }
-    }
-  }
+      showResult.value = true;
+    };
+
+    return {
+      types,
+      chosenType,
+      question,
+      option1,
+      option2,
+      option3,
+      option4,
+      showResult,
+      resultMessage,
+      getQuestion,
+      checkAnswer,
+    };
+  },
+};
 </script>
-  
+
 <style>
-  .searching {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+.quiz {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.quiz ul {
+  list-style: none;
+  padding: 0;
+}
+.quiz li {
+  cursor: pointer;
+  padding: 10px;
+  border: 1px solid #ccc;
+  margin: 5px;
+  transition: background-color 0.3s;
+}
+.quiz li:hover {
+  background-color: #f0f0f0;
+}
 </style>
-  
--- yu-gi-oh/src/views/HardCard.vue
+++ yu-gi-oh/src/views/HardCard.vue
@@ -2,9 +2,9 @@
 
     <div class = "container">
         
-        <button @click = "findCharacters"> Start </button>
+        <button v-if = "!gameStarted" @click = "findCharacters"> Begin </button>
 
-        <div v-if = "!GameOver"> 
+        <div v-if = "gameStarted && !Win && !Lose"> 
 
         <h2> What card matches this description? </h2>
 
@@ -17,6 +17,7 @@
         <button @click = "submitAnswer"> Match </button>
 
         </div>
+
     </div>
 
 </template>
@@ -26,6 +27,9 @@ import axios from 'axios'
 import { ref } from 'vue'
 
 export default{
+    components: {
+    
+    },
     setup() {
       
       const answer = ref('')
@@ -39,49 +43,79 @@ export default{
       const thirdAnswer = ref('')
       const fourthAnswer = ref('')
       
+      const loadFirst = ref(true)
+      const loadSecond = ref(false);
+      const loadThird = ref(false);
+      const loadFourth = ref(false);
+
       const firstCorrect = ref(false);
       const secondCorrect = ref(false);
       const thirdCorrect = ref(false);
       const fourthCorrect = ref(false);
-      const GameOver = ref(false);
+      const Lose = ref(false);
+      const Win = ref(false);
+      const gameStarted = ref(false);
 
       const findCharacters = async () => {
-        const response1 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
-        firstQuestion.value = response1.data.desc
-        firstAnswer.value = response1.data.name
-
-        const response2 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
-        secondQuestion.value = response2.data.desc
-        secondAnswer.value = response2.data.name
-
-        const response3 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
-        thirdQuestion.value = response3.data.desc
-        thirdAnswer.value = response3.data.name
-
-        const response4 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
-        fourthQuestion.value = response4.data.desc
-        fourthAnswer.value = response4.data.name
+        if(!gameStarted.value)
+        {
+          gameStarted.value = true;
+        }
+        if(loadFirst.value)
+        {
+            const response1 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
+            firstQuestion.value = response1.data.desc
+            firstAnswer.value = response1.data.name
+        }
+        else if(loadSecond.value)
+        {
+            const response2 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
+            secondQuestion.value = response2.data.desc
+            secondAnswer.value = response2.data.name
+        }
+        else if(loadThird.value)
+        {
+            const response3 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
+            thirdQuestion.value = response3.data.desc
+            thirdAnswer.value = response3.data.name
+        }
+        else if(loadFourth.value)
+        {
+            const response4 = await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
+            fourthQuestion.value = response4.data.desc
+            fourthAnswer.value = response4.data.name
+        }
       }
       
       function submitAnswer(){
         if(answer.value.toLowerCase() == firstAnswer.value.toLowerCase()){
             firstCorrect.value = true;
+            loadFirst.value = false;
+            loadSecond.value = true;
+            findCharacters();
         }
         else if(answer.value.toLowerCase() == secondAnswer.value.toLowerCase())
         {
             secondCorrect.value = true;
+            loadSecond.value = false;
+            loadThird.value = true;
+            findCharacters();
         }
         else if(answer.value.toLowerCase() == thirdAnswer.value.toLowerCase())
         {
             thirdCorrect.value = true;
+            loadThird.value = false;
+            loadFourth.value = true;
+            findCharacters();
         }
         else if(answer.value.toLowerCase() == fourthAnswer.value.toLowerCase())
         {
-            fourthAnswer.value = true;
+            fourthCorrect.value = true;
+            Win.value = true;
         }
         else
         {
-            GameOver.value = true;
+            Lose.value = true;
         }
       }
 
@@ -99,7 +133,9 @@ export default{
         thirdCorrect,
         fourthCorrect,
         answer,
-        GameOver,
+        Lose,
+        Win,
+        gameStarted,
         findCharacters,
         submitAnswer
       }
